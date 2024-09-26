55292450a8354675eaf499196f502da6f087935f
wangc <1264493255@qq.com>
2024-09-26 18:06:03 +0800

    init

---
+++ main.go
@@ -0,0 +1,6 @@
+// Copyright 2024 Innkeeper wangc <wangcheng.public@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/ischeng28/miniblog.
+
+package miniblog
